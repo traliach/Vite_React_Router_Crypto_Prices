c71e1d01cabe104f537ce9b7b0dc0453391ad9ce
traliach <ali.achille.traore@gmail.com>
2026-02-24 15:28:11 -0500

    add currencies list and links

--- src/pages/Currencies.jsx
+++ src/pages/Currencies.jsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom'
+
+const currencies = [
+  'BTC', 'ETH', 'LTC', 'XRP', 'BCH',
+  'ADA', 'DOT', 'LINK', 'XLM', 'DOGE',
+]
+
 function Currencies() {
-  return <h1>Currencies Page</h1>
+  return (
+    <div className="currencies">
+      <h1>Currencies</h1>
+      <ul>
+        {currencies.map((symbol) => (
+          <li key={symbol}>
+            <Link to={`/price/${symbol}`}>{symbol}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Currencies
