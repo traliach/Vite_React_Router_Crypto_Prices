ac074b929e8049a79a4dd62e4645e220675ea1d2
traliach <ali.achille.traore@gmail.com>
2026-02-24 15:26:34 -0500

    add routes and stub pages

---
+++ src/pages/Currencies.jsx
@@ -0,0 +1,5 @@
+function Currencies() {
+  return <h1>Currencies Page</h1>
+}
+
+export default Currencies
---
+++ src/pages/Price.jsx
@@ -0,0 +1,5 @@
+function Price() {
+  return <h1>Price Page</h1>
+}
+
+export default Price
